16808a34542145500acc96374c63d354ffe45c2a
Myles Borins <myles.borins@gmail.com>
2016-09-22 16:26:52 -0400

    looking good

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ summon the muse of song to serenade you as your scripts run
 ## usage
 
 mousai [Any Script as you would normally write it]
-mousai set path/to/song # set default song to be played
+mousai --set path/to/song # set default song to be played
+mousai --reset # reset to default song
 
 ---
 
--- bin/cmd.js
+++ bin/cmd.js
@@ -1,26 +1,85 @@
 #!/usr/bin/env node
 var path = require('path');
+var fs = require('fs');
 var spawn = require('child_process').spawn;
 
 var mousai = require('../lib');
 
 var appCfg = require('application-config');
+var chalk = require('chalk');
 var argv = require('minimist')(process.argv.slice(2));
 
 var command = argv._[0];
 var args = argv._.slice(1);
+var config = appCfg('node-mousai');
 
-var audioPath = path.join(__dirname, '../audio/elevator.mp3');
+var elevator = path.join(__dirname, '../audio/elevator.mp3');
 
-var playSong = mousai({
-  path: audioPath,
-  repeat: true
-});
+if (!command && !argv.set && !argv.reset) {
+  help();
+  process.exit(1);
+}
 
-var child = spawn(command, args, {
-  stdio: 'inherit'
-});
+if (argv.h || argv.help) {
+  help();
+  process.exit(0);
+}
 
-child.on('close', function () {
-  playSong.kill();
-});
+if (argv.set) {
+  var mp3Path = fs.realpathSync(argv.set);
+  config.write({
+    path: mp3Path
+  }, function (err) {
+    console.log(chalk.yellow('Default Set To: ') + mp3Path);
+  });
+}
+else if (argv.reset) {
+  config.write({
+    path: elevator
+  }, function () {
+    console.log(chalk.yellow('Default Reset To: ') + elevator);
+    return;
+  });
+}
+else {
+  config.read(afterConfigRead);
+}
+
+function help() {
+  console.log(chalk.yellow('\n    Usage') + ':\n\t$ mousai [some cmd and arguments] # play song while command runs\n\t$ mousa --set [path to mp3] # set default song\n');
+}
+
+function afterConfigRead(err, options) {
+  if (options.path) {
+    run(options);
+    return;
+  }
+  config.write({
+    path: elevator
+  }, function () {
+    run(options);
+    return;
+  });
+}
+
+function run(options) {
+  var playSong = mousai({
+    path: options.path,
+    repeat: true
+  });
+
+  var child = spawn(command, args, {
+    stdio: 'inherit'
+  });
+
+  child.on('error', function (err) {
+    if (err.code === 'ENOENT'); {
+      console.error(chalk.red('\nError:') + ' the command ' + chalk.yellow(command) + ' does not exist.\n');
+      process.exit(1);
+    }
+  });
+
+  child.on('close', function () {
+    playSong.kill();
+  });
+}
